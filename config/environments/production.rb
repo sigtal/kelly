e582fb5353603f4f51314f3ab91a256c8539163a
sigtal <sigtal0906@gmail.com>
2020-06-25 22:03:54 +0900

    try to heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "kelly_production"
 
   config.action_mailer.perform_caching = false
-
-  config.action_mailer.default_url_options = { :host => 'アプリ名.herokuapp.com' }
+  host = 'illustrator-kelly.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name      => ENV['SENDGRID_USERNAME'],
