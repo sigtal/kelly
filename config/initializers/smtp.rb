6f6fd543f9094e226fccc1ca0b65fb9333ba68fe
sigtal <sigtal0906@gmail.com>
2020-06-30 16:39:02 +0900

    sendgrid test7

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -5,5 +5,6 @@ ActionMailer::Base.smtp_settings = {
     user_name:            'apikey',
     password:             ENV['SENDGRID_API_KEY'], # 先ほど作成したアプリパスワード
     authentication:       'plain',
-    enable_starttls_auto: true
+    enable_starttls_auto: true,
+    openssl_verify_mode: 'peer'
  }
