ddafa920e05b3a12d72ccc5d13db8f519a0a3dd4
sigtal <sigtal0906@gmail.com>
2020-07-25 08:09:04 +0900

    image alt

--- app/models/illust.rb
+++ app/models/illust.rb
@@ -2,6 +2,7 @@ class Illust < ApplicationRecord
   has_one_attached :fullimage
   validate :image_presence
   validates :thumb, presence: true
+  validates :name, presence: true
 
   def image_presence
     errors.add(:fullimage, '画像がありません') unless fullimage.attached?
